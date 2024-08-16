a73b5597ba60b2af87e27e49f7ebf096e015b230
ManuelDiLena <manuel.dilena.29@gmail.com>
2024-08-16 19:53:36 +0200

    Section transitions

--- js/app.js
+++ js/app.js
@@ -10,8 +10,28 @@ function pageTransitions() {
       let currentBtn = document.querySelectorAll('.active-btn');
       currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
       this.className += ' active-btn';
-    })
-  }
+    });
+  };
+
+  // Sections active class
+  allSection.addEventListener('click', (e) => {
+    const id = e.target.dataset.id;
+    if (id) {
+      // remove selected from the other btns
+      sectBtns.forEach((btn) => {
+        btn.classList.remove('aactive');
+      });
+      e.target.classList.add('active');
+
+      // hide other sections
+      sections.forEach((section) => {
+        section.classList.remove('active');
+      });
+
+      const element = document.getElementById(id);
+      element.classList.add('active');
+    }
+  })
 }
 
 pageTransitions();
