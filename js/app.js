2e299e5fca60307e6892cc867c359a2378054b96
ManuelDiLena <manuel.dilena.29@gmail.com>
2024-08-21 19:27:32 +0200

    Timeline section

--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@ function pageTransitions() {
     if (id) {
       // remove selected from the other btns
       sectBtns.forEach((btn) => {
-        btn.classList.remove('aactive');
+        btn.classList.remove('active');
       });
       e.target.classList.add('active');
 
